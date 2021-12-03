eb4fd18d7ac3aa0d5de9c83a3a0cfab6b3d9a492
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2021-12-03 11:05:23 +0900

    Triggers: new_server

---
+++ fields.js
@@ -0,0 +1,9 @@
+module.exports = {
+  zoneChoices: [
+    { sample: "is1a", value: "is1a", label: "石狩第1ゾーン" },
+    { sample: "is1b", value: "is1b", label: "石狩第2ゾーン" },
+    { sample: "tk1a", value: "tk1a", label: "東京第1ゾーン" },
+    { sample: "tk1b", value: "tk1b", label: "東京第2ゾーン" },
+    { sample: "tk1v", value: "tk1v", label: "Sandbox" },
+  ],
+};
